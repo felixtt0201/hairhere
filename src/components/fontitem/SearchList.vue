373ff698c822dd03bd50651ad1b3a55d57c1715f
cin <chinnho1026@gmail.com>
2021-01-11 18:38:58 +0800

    新增查詢訂單

---
+++ src/components/fontitem/SearchList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container container-vh">
+    <h4 class="title-line w-100 text-center mt-5 mb-5 works-text-line">
+      查詢訂單
+    </h4>
+    <div class="search-content">
+      <form>
+        <label for="tel"
+          >電話<input type="tel" placeholder="請輸入電話號碼"
+        /></label>
+
+        <label for="email"
+          >信箱<input type="email" placeholder="請輸入信箱"
+        /></label>
+        <button type="button" class="btn">送出</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
